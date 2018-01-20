1e83c8529c795883b9b01d71548eb4c4a5117094
donghuanqing <donghuanqing@mobike.com>
2018-01-20 16:42:10 +0800

    add tikv

---
+++ tikv/src/lib.rs
@@ -0,0 +1,92 @@
+// Copyright 2016 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![crate_type = "lib"]
+#![cfg_attr(test, feature(test))]
+#![feature(fnbox)]
+#![feature(alloc)]
+#![feature(slice_patterns)]
+#![feature(box_syntax)]
+#![cfg_attr(feature = "dev", feature(plugin))]
+#![cfg_attr(feature = "dev", plugin(clippy))]
+#![cfg_attr(not(feature = "dev"), allow(unknown_lints))]
+#![recursion_limit = "100"]
+#![feature(ascii_ctype)]
+#![allow(module_inception)]
+#![allow(should_implement_trait)]
+#![allow(large_enum_variant)]
+#![allow(needless_pass_by_value)]
+#![allow(unreadable_literal)]
+#![allow(new_without_default_derive)]
+#![allow(verbose_bit_mask)]
+
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate quick_error;
+#[cfg(test)]
+extern crate test;
+extern crate protobuf;
+extern crate byteorder;
+extern crate rand;
+extern crate mio;
+extern crate tempdir;
+extern crate rocksdb;
+extern crate kvproto;
+extern crate time;
+extern crate tipb;
+extern crate libc;
+extern crate crc;
+extern crate alloc;
+extern crate chrono;
+extern crate zipf;
+#[macro_use]
+extern crate prometheus;
+#[macro_use]
+extern crate lazy_static;
+extern crate backtrace;
+extern crate url;
+extern crate fs2;
+extern crate regex;
+extern crate grpcio as grpc;
+extern crate fnv;
+extern crate flat_map;
+extern crate ordermap;
+extern crate futures;
+extern crate futures_cpupool;
+extern crate tokio_core;
+extern crate tokio_timer;
+extern crate serde_json;
+extern crate serde;
+extern crate murmur3;
+#[macro_use]
+extern crate serde_derive;
+#[cfg(test)]
+extern crate toml;
+extern crate sys_info;
+#[cfg(test)]
+extern crate utime;
+#[macro_use]
+extern crate fail;
+
+#[macro_use]
+pub mod util;
+pub mod config;
+pub mod raft;
+pub mod storage;
+pub mod raftstore;
+pub mod pd;
+pub mod server;
+pub mod coprocessor;
+
+pub use storage::Storage;
---
+++ tikv/src/util/worker/mod.rs
@@ -0,0 +1,370 @@
+// Copyright 2016 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+/// Worker contains all workers that do the expensive job in background.
+
+mod metrics;
+mod future;
+
+use std::sync::{Arc, Mutex};
+use std::thread::{self, Builder, JoinHandle};
+use std::io;
+use std::fmt::{self, Debug, Display, Formatter};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::mpsc::{self, Receiver, SendError, Sender};
+use std::error::Error;
+
+use util::time::SlowTimer;
+use self::metrics::*;
+
+pub use self::future::Runnable as FutureRunnable;
+pub use self::future::Scheduler as FutureScheduler;
+pub use self::future::Worker as FutureWorker;
+
+pub struct Stopped<T>(pub T);
+
+impl<T> Display for Stopped<T> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "channel has been closed")
+    }
+}
+
+impl<T> Debug for Stopped<T> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "channel has been closed")
+    }
+}
+
+impl<T> From<Stopped<T>> for Box<Error + Sync + Send + 'static> {
+    fn from(_: Stopped<T>) -> Box<Error + Sync + Send + 'static> {
+        box_err!("channel has been closed")
+    }
+}
+
+pub trait Runnable<T: Display> {
+    fn run(&mut self, t: T);
+    fn shutdown(&mut self) {}
+}
+
+pub trait BatchRunnable<T: Display> {
+    /// run a batch of tasks.
+    ///
+    /// Please note that ts will be clear after invoking this method.
+    fn run_batch(&mut self, ts: &mut Vec<T>);
+    fn shutdown(&mut self) {}
+}
+
+impl<T: Display, R: Runnable<T>> BatchRunnable<T> for R {
+    fn run_batch(&mut self, ts: &mut Vec<T>) {
+        for t in ts.drain(..) {
+            let task_str = format!("{}", t);
+            let timer = SlowTimer::new();
+            self.run(t);
+            slow_log!(timer, "handle task {}", task_str);
+        }
+    }
+
+    fn shutdown(&mut self) {
+        Runnable::shutdown(self)
+    }
+}
+
+/// Scheduler provides interface to schedule task to underlying workers.
+pub struct Scheduler<T> {
+    name: Arc<String>,//DHQ: Arc的clone，只产生一个新的pointer，不copy数据区
+    counter: Arc<AtomicUsize>,
+    sender: Sender<Option<T>>,
+}
+
+impl<T: Display> Scheduler<T> {
+    fn new<S: Into<String>>(
+        name: S,
+        counter: AtomicUsize,
+        sender: Sender<Option<T>>,//DHQ: Sender用到了T
+    ) -> Scheduler<T> {
+        Scheduler {
+            name: Arc::new(name.into()),
+            counter: Arc::new(counter),
+            sender: sender,
+        }
+    }
+
+    /// Schedule a task to run.
+    ///
+    /// If the worker is stopped, an error will return.
+    pub fn schedule(&self, task: T) -> Result<(), Stopped<T>> {
+        debug!("scheduling task {}", task);
+        if let Err(SendError(Some(t))) = self.sender.send(Some(task)) {
+            return Err(Stopped(t));
+        }
+        self.counter.fetch_add(1, Ordering::SeqCst);
+        WORKER_PENDING_TASK_VEC
+            .with_label_values(&[&self.name])
+            .inc();
+        Ok(())
+    }
+
+    /// Check if underlying worker can't handle task immediately.
+    pub fn is_busy(&self) -> bool {
+        self.counter.load(Ordering::SeqCst) > 0
+    }
+}
+
+impl<T: Display> Clone for Scheduler<T> {
+    fn clone(&self) -> Scheduler<T> {//DHQ:Clone是个trait，不要求copy所有的field。
+        Scheduler {
+            name: self.name.clone(),
+            counter: self.counter.clone(),
+            sender: self.sender.clone(),
+        }
+    }
+}
+
+/// Create a scheduler that can't be scheduled any task.
+///
+/// Useful for test purpose.
+#[cfg(test)]
+pub fn dummy_scheduler<T: Display>() -> Scheduler<T> {
+    let (tx, _) = mpsc::channel();
+    Scheduler::new("dummy scheduler", AtomicUsize::new(0), tx)
+}
+
+/// A worker that can schedule time consuming tasks.
+pub struct Worker<T: Display> {
+    scheduler: Scheduler<T>,
+    receiver: Mutex<Option<Receiver<Option<T>>>>,//DHQ: TODO: 为什么需要一个Mutex?
+    handle: Option<JoinHandle<()>>,
+}
+
+fn poll<R, T>(mut runner: R, rx: Receiver<Option<T>>, counter: Arc<AtomicUsize>, batch_size: usize)
+where
+    R: BatchRunnable<T> + Send + 'static,
+    T: Display + Send + 'static,
+{
+    let name = thread::current().name().unwrap().to_owned();
+    let mut keep_going = true;
+    let mut buffer = Vec::with_capacity(batch_size);
+    while keep_going {
+        let t = rx.recv();//
+        match t {
+            Ok(Some(t)) => buffer.push(t),
+            _ => break,
+        }
+        while buffer.len() < batch_size {
+            match rx.try_recv() {
+                Ok(None) => {
+                    keep_going = false;
+                    break;
+                }
+                Ok(Some(t)) => buffer.push(t),//DHQ:不是直接处理，而是放到buffer(vector)中,下面run_batch
+                _ => break,
+            }
+        }
+        counter.fetch_sub(buffer.len(), Ordering::SeqCst);
+        WORKER_PENDING_TASK_VEC
+            .with_label_values(&[&name])
+            .sub(buffer.len() as f64);
+        WORKER_HANDLED_TASK_VEC
+            .with_label_values(&[&name])
+            .inc_by(buffer.len() as f64)
+            .unwrap();
+        runner.run_batch(&mut buffer);//runner有BatchRunnable trait，调用其run_batch, 本文件中
+        buffer.clear();
+    }
+    runner.shutdown();
+}
+
+impl<T: Display + Send + 'static> Worker<T> {
+    /// Create a worker.
+    pub fn new<S: Into<String>>(name: S) -> Worker<T> {
+        let (tx, rx) = mpsc::channel();
+        Worker {
+            scheduler: Scheduler::new(name, AtomicUsize::new(0), tx),//DHQ: 就是本文件前面定义的Scheduler的new()
+            receiver: Mutex::new(Some(rx)),//DHQ: Mutex::new()，而不是直接使用了
+            handle: None,
+        }
+    }
+
+    /// Start the worker.
+    pub fn start<R: Runnable<T> + Send + 'static>(&mut self, runner: R) -> Result<(), io::Error> {
+        self.start_batch(runner, 1)
+    }
+
+    pub fn start_batch<R>(&mut self, runner: R, batch_size: usize) -> Result<(), io::Error>
+    where
+        R: BatchRunnable<T> + Send + 'static,
+    {
+        let mut receiver = self.receiver.lock().unwrap();//DHQ: lock，unwrap出receiver,这个函数调用会并发么？
+        info!("starting working thread: {}", self.scheduler.name);
+        if receiver.is_none() {
+            warn!("worker {} has been started.", self.scheduler.name);
+            return Ok(());
+        }
+
+        let rx = receiver.take().unwrap();
+        let counter = self.scheduler.counter.clone();
+        let h = Builder::new()
+            .name(thd_name!(self.scheduler.name.as_ref()))
+            .spawn(move || poll(runner, rx, counter, batch_size))?; //DHQ: poll, worker/mod.rs中，循环recv，调用runner处理. rx的ownership应该就move给thread了
+        self.handle = Some(h);
+        Ok(())
+    }
+
+    /// Get a scheduler to schedule task.
+    pub fn scheduler(&self) -> Scheduler<T> {
+        self.scheduler.clone()
+    }
+
+    /// Schedule a task to run.
+    ///
+    /// If the worker is stopped, an error will return.
+    pub fn schedule(&self, task: T) -> Result<(), Stopped<T>> {
+        self.scheduler.schedule(task)
+    }
+
+    /// Check if underlying worker can't handle task immediately.
+    pub fn is_busy(&self) -> bool {
+        self.handle.is_none() || self.scheduler.is_busy()
+    }
+
+    pub fn name(&self) -> &str {
+        self.scheduler.name.as_str()
+    }
+
+    /// Stop the worker thread.
+    pub fn stop(&mut self) -> Option<thread::JoinHandle<()>> {
+        // close sender explicitly so the background thread will exit.
+        info!("stoping {}", self.scheduler.name);
+        if self.handle.is_none() {
+            return None;
+        }
+        if let Err(e) = self.scheduler.sender.send(None) {
+            warn!("failed to stop worker thread: {:?}", e);
+        }
+        self.handle.take()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::thread;
+    use std::sync::*;
+    use std::sync::mpsc::*;
+    use std::time::Duration;
+
+    use super::*;
+
+    struct StepRunner {
+        ch: Sender<u64>,
+    }
+
+    impl Runnable<u64> for StepRunner {
+        fn run(&mut self, step: u64) {
+            self.ch.send(step).unwrap();
+            thread::sleep(Duration::from_millis(step));
+        }
+
+        fn shutdown(&mut self) {
+            self.ch.send(0).unwrap();
+        }
+    }
+
+    struct BatchRunner {
+        ch: Sender<Vec<u64>>,
+    }
+
+    impl BatchRunnable<u64> for BatchRunner {//DHQ: 这段是测试用的
+        fn run_batch(&mut self, ms: &mut Vec<u64>) {
+            self.ch.send(ms.to_vec()).unwrap();
+        }
+
+        fn shutdown(&mut self) {
+            self.ch.send(vec![]).unwrap();
+        }
+    }
+
+    #[test]
+    fn test_worker() {
+        let mut worker = Worker::new("test-worker");
+        let (tx, rx) = mpsc::channel();
+        worker.start(StepRunner { ch: tx }).unwrap();
+        assert!(!worker.is_busy());
+        worker.schedule(60).unwrap();
+        worker.schedule(40).unwrap();
+        worker.schedule(50).unwrap();
+        assert!(worker.is_busy());
+        assert_eq!(rx.recv_timeout(Duration::from_secs(3)).unwrap(), 60);
+        assert_eq!(rx.recv_timeout(Duration::from_secs(3)).unwrap(), 40);
+        assert_eq!(rx.recv_timeout(Duration::from_secs(3)).unwrap(), 50);
+        assert!(!worker.is_busy());
+        worker.stop().unwrap().join().unwrap();
+        // now worker can't handle any task
+        assert!(worker.is_busy());
+        // when shutdown, StepRunner should send back a 0.
+        assert_eq!(0, rx.recv().unwrap());
+    }
+
+    #[test]
+    fn test_threaded() {
+        let mut worker = Worker::new("test-worker-threaded");
+        let (tx, rx) = mpsc::channel();
+        worker.start(StepRunner { ch: tx }).unwrap();
+        let scheduler = worker.scheduler();
+        thread::spawn(move || {
+            scheduler.schedule(90).unwrap();
+            scheduler.schedule(110).unwrap();
+        });
+        assert_eq!(rx.recv_timeout(Duration::from_secs(3)).unwrap(), 90);
+        assert_eq!(rx.recv_timeout(Duration::from_secs(3)).unwrap(), 110);
+        worker.stop().unwrap().join().unwrap();
+        assert_eq!(0, rx.recv().unwrap());
+    }
+
+    #[test]
+    fn test_batch() {
+        let mut worker = Worker::new("test-worker-batch");
+        let (tx, rx) = mpsc::channel();
+        worker.start_batch(BatchRunner { ch: tx }, 10).unwrap();
+        for _ in 0..20 {
+            worker.schedule(50).unwrap();
+        }
+        worker.stop().unwrap().join().unwrap();
+        let mut sum = 0;
+        loop {
+            let v = rx.recv_timeout(Duration::from_secs(3)).unwrap();
+            // when runner is shutdown, it will send back an empty vector.
+            if v.is_empty() {
+                break;
+            }
+            sum += v.into_iter().fold(0, |a, b| a + b);
+        }
+        assert_eq!(sum, 50 * 20);
+        assert!(rx.recv().is_err());
+    }
+
+    #[test]
+    fn test_autowired_batch() {
+        let mut worker = Worker::new("test-worker-batch");
+        let (tx, rx) = mpsc::channel();
+        worker.start_batch(StepRunner { ch: tx }, 10).unwrap();
+        for _ in 0..20 {
+            worker.schedule(50).unwrap();
+        }
+        worker.stop().unwrap().join().unwrap();
+        for _ in 0..20 {
+            rx.recv_timeout(Duration::from_secs(3)).unwrap();
+        }
+        assert_eq!(rx.recv().unwrap(), 0);
+    }
+}
